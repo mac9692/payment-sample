9212beeda194825d08802a88b1b065d927af81bc
박진성 <mac9692@nate.com>
2025-02-11 17:16:49 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: {enabled: true},
+  sourcemap: true,
+  runtimeConfig: {
+    public: {
+      PAYMENT: {
+        NICE_PAY: {
+          MID: process.env.NICE_PAY_MID,
+          MOID: process.env.NICE_PAY_MOID,
+          MERCHANT_KEY: process.env.NICE_PAY_MERCHANT_KEY
+        }
+      }
+    }
+  },
+})
